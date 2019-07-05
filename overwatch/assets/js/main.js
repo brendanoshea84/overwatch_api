83e197c6642eb032b36333d41b01fa822a332b50
brendanoshea84 <b_oshea_84@hotmail.com>
2019-07-05 11:42:57 +0200

    using jquery to access the json file

---
+++ overwatch/assets/js/main.js
@@ -0,0 +1,17 @@
+var container = $('div.container');
+
+$('input#get').click(function() {
+    $.ajax({
+        type: 'GET', 
+        url: 'assets/js/info.json',
+        dataType: 'json', 
+        success: function(data) {
+            $.each(data, function(index, item) {
+                $.each(item, function(key, value) {
+                    console.log(key, value.name);
+                });
+                container.append('<br></br>');
+            });
+        }
+    });
+})

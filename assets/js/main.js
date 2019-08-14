bf03a6cb60860aa5bc0bd719276c2b249d167e18
brendanoshea84 <b_oshea_84@hotmail.com>
2019-08-14 07:51:11 +0200

    fixing folder problems

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ xhttp.onreadystatechange = function () {
         //output goes into collapse area in "Character Icon" area
         var output = '';
         for (var i = 0; i < characters.length; i++) {
-            output += '<div class="characterBox' +' '+ characters[i].role+'">'+ '<img src="' + characters[i].image + '"' + 'width="100%" height="100%"' +'class="'+characters[i].role + '"/>' + '<name ="'+characters[i].name +'">'+'"</div>';
+            output += '<div class="characterBox' +' '+ characters[i].role+'" id="'+characters[i].id+'">'+ '<img src="' + characters[i].image + '"' + 'width="100%" height="100%"' +'class="'+characters[i].role + '"/>' + '<name ="'+characters[i].name +'">'+'"</div>';
         }
         document.getElementById('innerPicter').innerHTML = output;
 
@@ -80,6 +80,6 @@ $('#support-toggle').click(function(){
 
 //Single player information area
 
-$("#characterBox").click(function(){
-    console.log("worki");
-})
+var info = JSON.parse("xhttp");
+
+console.log(info);
